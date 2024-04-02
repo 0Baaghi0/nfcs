22f42f261b7aba24bc701944627a1d91b4bfdb95
Durgamani123 <durgamani147@gmail.com>
2024-04-02 09:10:03 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'LoginFormofNFCS';
+  loginForm:any=new FormGroup({})
+
+  constructor(private formbuilder:FormBuilder){
+  
+  }
+
+
+
+  ngOnInit(): void {
+    this.FormIntalization()
+    
+  }
+
+  FormIntalization(){
+    this.loginForm=this.formbuilder.group({
+      userName:['',Validators.required],
+      password:['',Validators.required]
+    })
+
+  }
 }
