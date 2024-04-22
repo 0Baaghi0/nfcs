95f4199fe07e06180f2b868c2e1421f4b7fa8d57
rajeev <Rajeev.Rai@hritfactory.com>
2024-04-22 18:48:06 +0530

    profile page css

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { EmailValidator } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,10 +8,13 @@ import { EmailValidator } from '@angular/forms';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  constructor(private router:Router){}
 
   name: string= "Sowmya"
   email: string= "sowmya.n@northfacein.com"
   
-
+  backToLogin(){
+    this.router.navigate([''])
+  }
 
 }
